e6830484bf867e43f53d2b0689fc29cb233da1d6
lemuruz <nakabodeepatiparn@gmail.com>
2025-12-14 16:59:44 +0700

    complete yt scrapers

--- app/scrapers/youtube.py
+++ app/scrapers/youtube.py
@@ -1,111 +1,103 @@
 from datetime import datetime, timedelta, timezone
-from typing import List, Optional
+from typing import Optional
 import feedparser
+from pydantic import BaseModel
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api._errors import TranscriptsDisabled, NoTranscriptFound
 
+class Transcript(BaseModel):
+    text: str 
 
-def get_rss_url(channel_id: str) -> str:
-    """
-    Build YouTube RSS feed URL from channel ID
-    """
-    return f"https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}"
-
-
-def extract_vid_url(video_id: str) -> str:
-    """
-    Convert video ID to full YouTube URL
-    """
-    return f"https://www.youtube.com/watch?v={video_id}"
-
-
-# def detect_live(entry) -> bool:
-#     if "live" in entry.title.lower():
-#         print(1)
-#     elif "stream" in entry.title.lower():
-#         print(2)
-#     elif getattr(entry, "yt_livebroadcastcontent", "") == "live":
-#         print(3)
-#     return (
-#         "live" in entry.title.lower()
-#         or "stream" in entry.title.lower()
-#         or getattr(entry, "yt_livebroadcastcontent", "") == "live"
-#     )
-# def classify_video_type(entry) -> str:
-#     if detect_live(entry):
-#         return "live"
-#     return "video"
+class ChannelVideo(BaseModel):
+    video_id: str
+    channel_id: str
+    title: str
+    url: str
+    description: str
+    published_at: datetime
+    transcript : Optional[str] = None
 
 
 
-def get_latest_vids(channel_id: str, hours: int = 24) -> list[dict]:
-    """
-    Fetch latest videos from a YouTube channel RSS feed
-    within the last `hours`
-    """
-    rss_url = get_rss_url(channel_id)
-    feed = feedparser.parse(rss_url)
+class YouTubeScraper:
+    def __init__(self):
+        self.transcript_api = YouTubeTranscriptApi()
 
-    cutoff = datetime.now(timezone.utc) - timedelta(hours=hours)
-    videos = []
+    @staticmethod
+    def get_rss_url(channel_id: str) -> str:
+        return f"https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}"
 
-    for entry in feed.entries:
-        published = datetime.fromisoformat(
-            entry.published.replace("Z", "+00:00")
-        )
+    @staticmethod
+    def extract_video_url(video_id: str) -> str:
+        return f"https://www.youtube.com/watch?v={video_id}"
 
-        if published >= cutoff:
+    def get_latest_videos(self,channel_id: str, hours: int = 24,) -> list[dict]:
+        rss_url = self.get_rss_url(channel_id)
+        feed = feedparser.parse(rss_url)
+
+        cutoff = datetime.now(timezone.utc) - timedelta(hours=hours)
+        videos = []
+
+        for entry in feed.entries:
+            published_at = datetime.fromisoformat(
+                entry.published.replace("Z", "+00:00")
+            )
+
+            if published_at < cutoff:
+                continue
+
             video_id = entry.yt_videoid
 
-            videos.append({
-                "video_id": video_id,
-                "title": entry.title,
-                "published": published,
-                "url": extract_vid_url(video_id),
-                # "type": classify_video_type(entry),
-            })
+            videos.append(
+                ChannelVideo(
+                    video_id=video_id,
+                    channel_id=channel_id,
+                    title=entry.title,
+                    url=self.extract_video_url(video_id),
+                    description=getattr(entry, "description", ""),
+                    published_at=published_at,
+                )
+            )
 
-    return videos
-
-
-def get_transcript(video_id: str) -> Optional[str]:
-    """
-    Fetch transcript text for a YouTube video.
-    Returns None if transcript is unavailable.
-    """
-    try:
-        ytt_api  = YouTubeTranscriptApi()
-        transcript = ytt_api.fetch(video_id)
-        return " ".join([item.text for item in transcript])
-    except Exception:
-        return None
+        return videos
 
 
-def scrape_channel(channel_id: str, hours: int = 24) -> list[dict]:
-    """
-    Scrape a YouTube channel for recent videos
-    and attach transcripts
-    """
-    videos = get_latest_vids(channel_id, hours)
+    def get_transcript(self,video_id: str) -> Optional[str]:
 
-    for video in videos:
-        temp = get_transcript(video["video_id"])
-        # print(temp)
-        video["transcript"] = temp
+        try:
+            ytt_api  = YouTubeTranscriptApi()
+            transcript = ytt_api.fetch(video_id)
+            return " ".join([item.text for item in transcript])
+        except (TranscriptsDisabled, NoTranscriptFound):
+            return None
+        except Exception as e:
+            print("Transcript error:", type(e).__name__, e)
+            return None
 
-    return videos
+
+    def scrape_channel(self,channel_id: str, hours: int = 24) -> list[dict]:
+
+        videos = self.get_latest_videos(channel_id, hours)
+
+        for video in videos:
+            video.transcript = self.get_transcript(video.video_id)
+
+        return videos
 
 if __name__ == "__main__":
+    scraper = YouTubeScraper()
     CHANNEL_ID = "UCUzqsHTec5ZkJU5S4Tmhweg"
 
-    data = scrape_channel(CHANNEL_ID, hours=144)
+    channel_vids:list[ChannelVideo] = scraper.scrape_channel(CHANNEL_ID, hours=144)
 
-    for video in data:
+    for video in channel_vids:
         print("-" * 50)
-        # print(video["type"])
-        print(video["title"])
-        print(video["url"],"ID =",video["video_id"])
-        print(video["published"])
-        print(video["transcript"][:200]+"...." if video["transcript"] else "No transcript")
-        # print("-" * 50)
+        print(video.title)
+        print(video.url, "ID =", video.video_id)
+        print(video.published_at)
+        print(
+            video.transcript[:200] + "..."
+            if video.transcript
+            else "No transcript"
+        )
     print("-" * 50)
